75623a4335949dabba789853bc8efd40fb040b1f
Jacques <jacques.louis.sc@gmail.com>
2022-09-18 16:02:23 +0200

    CHAT-17 Create message post request

--- src/app/services/message/message.service.ts
+++ src/app/services/message/message.service.ts
@@ -1,12 +1,21 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Message} from '../../common/interfaces/message.interface';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
+  // TODO: put serverUrl in an config file
+  private serverUrl = 'localhost:8080';
+  private chatUrl = `${this.serverUrl}/messages`;
+
   constructor(private http: HttpClient) {
   }
 
+  public sendMessage(msg: Message): Observable<Message> {
+    return this.http.post<Message>(this.chatUrl, msg);
+  }
 }
