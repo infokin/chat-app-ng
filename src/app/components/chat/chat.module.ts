962c98db97571ab382fef4eee539ebbe90c93ebe
Jacques <jacques.louis.sc@gmail.com>
2022-09-25 18:12:14 +0200

    CHAT-18 Add textarea and button

--- src/app/components/chat/chat.module.ts
+++ src/app/components/chat/chat.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { ChatComponent } from './chat.component';
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
 
 @NgModule({
   declarations: [
     ChatComponent
+  ],
+  imports: [
+    ButtonModule,
+    InputTextareaModule
   ],
   exports: [ChatComponent]
 })
