7a939f66243be81173181c2d60942446efe578c8
Jacques <jacques.louis.sc@gmail.com>
2022-10-01 12:43:49 +0200

    CHAT-17 Remove code from app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import {Component} from '@angular/core';
-import {MessageService} from './services/message/message.service';
-import {Message} from './models';
 
 @Component({
   selector: "chat-app-root",
@@ -9,18 +7,4 @@ import {Message} from './models';
 })
 export class AppComponent {
   title = 'chat-app-ng';
-
-  constructor(
-    private messageService: MessageService
-  ) {
-
-    // TODO: Testing
-    const message: Message = new Message();
-    message.setContent('Test message..');
-    this.messageService.sendMessage(message)
-      .subscribe((message: Message) => {
-        console.log('No idea what that is: ', message)
-      })
-
-  }
 }
