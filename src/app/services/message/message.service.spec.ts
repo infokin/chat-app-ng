05beed9031d156c202305cefd2f5afbc7a02ed48
Jacques <jacques.louis.sc@gmail.com>
2022-11-22 21:04:00 +0100

    CHAT-17 Fix message service test

--- src/app/services/message/message.service.spec.ts
+++ src/app/services/message/message.service.spec.ts
@@ -1,12 +1,20 @@
 import { TestBed } from '@angular/core/testing';
 
 import { MessageService } from './message.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoggerTestingModule } from 'ngx-logger/testing';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        LoggerTestingModule,
+      ]
+    });
     service = TestBed.inject(MessageService);
   });
 
