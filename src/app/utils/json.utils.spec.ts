af287ce1df9ab8d0d3be8a30f63840ba02d1fd5f
Jacques <jacques.louis.sc@gmail.com>
2022-11-22 23:05:43 +0100

    CHAT-17 Add serialize test

--- src/app/utils/json.utils.spec.ts
+++ src/app/utils/json.utils.spec.ts
@@ -1,8 +1,13 @@
 import { JsonUtils } from './json-utils';
+import { Message } from '../models';
 
 describe('JsonUtils', (): void => {
-  it('should create', (): void => {
-    // @ts-ignore
-    expect(new JsonUtils()).toBeTruthy();
+  it('should serialize', (): void => {
+    const message: Message = new Message();
+    const content: string = 'this is a message'
+    message.setContent(content);
+
+    const serializedMessage: string = JsonUtils.serialize(message, Message);
+    expect(serializedMessage).toContain(content);
   });
 })
