7c1a905b4303d18a0e345c2f92c9c94e37ac02b4
Jacques <jacques.louis.sc@gmail.com>
2022-11-22 21:52:11 +0100

    CHAT-17 Add test for json utils

---
+++ src/app/utils/json.utils.spec.ts
@@ -0,0 +1,8 @@
+import { JsonUtils } from './json-utils';
+
+describe('JsonUtils', (): void => {
+  it('should create', (): void => {
+    // @ts-ignore
+    expect(new JsonUtils()).toBeTruthy();
+  });
+})
