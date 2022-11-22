37ed7eb744a7e1c97e16e7860710c2ce7c5fb892
Jacques <jacques.louis.sc@gmail.com>
2022-11-22 23:16:58 +0100

    CHAT-17 Add deserialize test

--- src/app/utils/json.utils.spec.ts
+++ src/app/utils/json.utils.spec.ts
@@ -1,6 +1,10 @@
 import { JsonUtils } from './json-utils';
 import { Message } from '../models';
 
+interface IMessage {
+  content: string;
+}
+
 describe('JsonUtils', (): void => {
   it('should serialize', (): void => {
     const message: Message = new Message();
@@ -10,4 +14,22 @@ describe('JsonUtils', (): void => {
     const serializedMessage: string = JsonUtils.serialize(message, Message);
     expect(serializedMessage).toContain(content);
   });
+
+  it('should deserialize', (): void => {
+    const msg1: IMessage = {
+      content: 'message one'
+    };
+
+    const msg2: IMessage = {
+      content: 'message two'
+    };
+
+    const input: IMessage[] = [msg1, msg2];
+    const result: Message[] = JsonUtils.deserializeArray<Message>(input, Message);
+
+    expect(result.length).toEqual(input.length);
+    result.forEach((value: Message, index: number) => {
+      expect(value.getContent()).toEqual(input[index]?.content)
+    })
+  })
 })
