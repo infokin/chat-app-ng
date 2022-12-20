46561ec30e7cc69728c2a164d15c5ba1b5e57b72
Jacques <jacques.louis.sc@gmail.com>
2022-12-20 14:57:49 +0100

    CHAT-18 Use list instead of textarea to show messages

--- src/app/components/chat/show-messages/show-messages.component.ts
+++ src/app/components/chat/show-messages/show-messages.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { delay, from, Observable, of } from 'rxjs';
 
+export interface Message {
+  content: string;
+}
 @Component({
   selector: 'app-chat-show-messages',
   templateUrl: './show-messages.component.html',
   styleUrls: ['./show-messages.component.scss']
 })
-export class ShowMessagesComponent {
+export class ShowMessagesComponent implements OnInit {
 
+  public messages: Message[] = [];
+
+  public ngOnInit(): void {
+    of(
+      {content: 'Hi'},
+      {content: 'Hello'},
+      {content: 'Bye!'},
+      )
+      .subscribe({
+        next: (msg: Message) => this.messages.push(msg),
+        error: (err: unknown) => console.log(err),
+      });
+  }
 }
--- src/app/components/chat/show-messages/show-messages.module.ts
+++ src/app/components/chat/show-messages/show-messages.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { ShowMessagesComponent } from './show-messages.component';
 import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ListboxModule } from 'primeng/listbox';
+import { AsyncPipe } from '@angular/common';
 
 @NgModule({
   declarations: [
     ShowMessagesComponent
   ],
   imports: [
-    InputTextareaModule
+    InputTextareaModule,
+    ListboxModule,
+    AsyncPipe
   ],
   exports: [ShowMessagesComponent],
 })
