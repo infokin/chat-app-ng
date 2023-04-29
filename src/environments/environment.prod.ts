da6fcc0d211fb7fad5c733ed7edc691bcbbd7221
Johannes Hillert <johannes.hillert@infokin.org>
2023-04-29 15:10:55 +0200

    CHAT-18 Adjust message service URL in environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,5 @@ import {Environment} from "../app/common/interfaces";
 
 export const environment: Environment = {
   production: true,
-  serverUrl: "http://localhost:8080",
-  messageServiceUrl: "http://localhost:8080/messages"
+  messageServiceUrl: "/messages"
 };
